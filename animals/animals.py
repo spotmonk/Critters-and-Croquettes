edd9ffebe908f72695f43880a2dea5894bcbf5b6
Geno McNew <genemcnew3@gmail.com>
2020-10-17 11:36:59 -0500

    created classes and instances of animals and exported them

---
+++ animals/animals.py
@@ -0,0 +1,173 @@
+from datetime import date
+
+class Llama:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+        self.shift = "morning"
+
+class Goat:
+
+    def __init__(self,name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+        self.shift = "morning"
+
+class Donkey:
+
+    def __init__(self,name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+        self.shift = "afteroon"
+
+class Alpaca:
+
+    def __init__(self,name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+        self.shift = "afternoon"
+
+class Sheep:
+
+    def __init__(self, name, species ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+        self.shift = "midday"
+
+class Corn_Snake:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Gopher_Snake:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class California_Kingsnake:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Rosy_Boa:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Ball_Python:
+
+    def __init__(self,name, species ):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.slithering = True
+
+class Weather_loach:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Guppy:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Tetra:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Arrowana:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+class Clown:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.swimming = True
+
+
+miss_Llama = Llama("Lucy", "domestic llama")
+mr_Goat = Goat("Genermy", "screaming goat")
+mr_Donkey = Donkey("Donny", "domestic donkey")
+miss_Alpaca = Alpaca("Allie", "wooly alpaca")
+miss_Sheep = Sheep("Dolly", "cloned sheep")
+mr_Corn_Snake = Corn_Snake("Cornelius", "nope noodle")
+miss_Gopher_Snake = Gopher_Snake("Gophey", "nope noodle")
+mr_California_Kingsnake = California_Kingsnake("Arnold", "nope noodle")
+miss_Rosy_Boa = Rosy_Boa("Rose", "nope noodle")
+mr_Ball_Python = Ball_Python("Luci", "nope noodle")
+miss_Weather_loach = Weather_loach("Lochie", "freshwater fish")
+mr_Guppy = Guppy("Gupert", "freshwater fish")
+miss_Tetra = Tetra("Tetty", "freshwater fish")
+miss_Arrowana = Arrowana("Rowana", "freshwater fish")
+mr_Clown = Clown("Nemo", "saltwater fish")
